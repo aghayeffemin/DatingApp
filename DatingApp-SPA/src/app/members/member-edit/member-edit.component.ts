99bd2f3d5b90a2a25c03a5fcb37ec88825e62f5a
Emin Agayev Fexreddin oglu <EminAg@emdkdedrx.gov.az>
2019-01-06 21:58:23 -0800

    updateUser

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -20,7 +22,7 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService, private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data=>{
@@ -29,8 +31,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success("Profile updated successfully!");
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next =>{
+      this.alertify.success("Profile updated successfully!");
+      this.editForm.reset(this.user);
+    },error=>{
+      this.alertify.error(error);
+    });
   }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -14,18 +14,21 @@ namespace DatingApp.API.Helpers
                 {
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
                 })
-                .ForMember(dest => dest.Age, opt=>{
-                    opt.ResolveUsing(d=>d.DateofBirth.CalculateAge());
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d => d.DateofBirth.CalculateAge());
                 });
             CreateMap<User, UserForDetailedDto>()
                 .ForMember(dest => dest.PhotoUrl, opt =>
                 {
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
                 })
-                .ForMember(dest => dest.Age, opt=>{
-                    opt.ResolveUsing(d=>d.DateofBirth.CalculateAge());
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d => d.DateofBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
